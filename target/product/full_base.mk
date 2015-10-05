f3746ba4efa9928a5bc9eeecd9af4b22d0088a0b
Chet Kener <Cl3Kener@gmail.com>
2015-10-05 19:29:03 -0400

    Debloat full_base.mk
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -21,19 +21,12 @@
 
 PRODUCT_PACKAGES := \
     libfwdlockengine \
-    OpenWnn \
-    libWnnEngDic \
-    libWnnJpnDic \
-    libwnndict \
     WAPPushManager
 
 PRODUCT_PACKAGES += \
     Galaxy4 \
-    HoloSpiralWallpaper \
     LiveWallpapers \
     LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
     PhaseBeam \
     PhotoTable
 
