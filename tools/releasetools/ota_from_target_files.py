f4bc893e2138b7fba9723c067ac29744b7bb4fcb
mydongistiny <jaysonedson@gmail.com>
2016-01-05 13:16:43 -0800

    build: Remove annoying text when flashing
    Change-Id: If370c74000754abd51dfde221f4146bbf75f2327
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -625,8 +625,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 """ % bcb_dev)
 
   # Dump fingerprints
-  script.Print("Target: %s" % CalculateFingerprint(
-      oem_props, oem_dict, OPTIONS.info_dict))
+  #script.Print("Target: %s" % CalculateFingerprint(
+  #    oem_props, oem_dict, OPTIONS.info_dict))
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
